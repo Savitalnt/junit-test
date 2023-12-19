775cd7d29db9b2bd8d2ed273ceeac28438c7047c
Savitalnt <111843797+Savitalnt@users.noreply.github.com>
2023-12-19 15:48:13 +0530

    Update test.java

--- src/test/java/com/javainuse/test/test.java
+++ src/test/java/com/javainuse/test/test.java
@@ -1,39 +1,52 @@
-package com.javainuse.test;
+ package com.javainuse.test;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+public class test{
 
-
-public class test extends SpringBootHelloWorldTests {
-
-	// @Autowired
-	// private WebApplicationContext webApplicationContext;
-
-	// private MockMvc mockMvc;
-
-	// @Before
-	// public void setup() {
-	// 	mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	// }
-
-	// @Test
-	// public void testEmployee() throws Exception {
-	// 	mockMvc.perform(get("/employee")).andExpect(status().isOk())
-	// 			.andExpect(content().contentType("application/json;charset=UTF-8"))
-	// 			.andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
-	// 			.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
-
-	// }
-
-  @Test
-public void givenNumber_whenEven_thenTrue() {
-	int number=12;
-    assertTrue(number % 2 == 0);
+	@Test
+	public void test(){
+		addition t=new addition();
+		int actual = t.sum(10,20);
+		int expected=30;
+		assertEqual(expected,actual);
+	}
 }
-}
+
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+// import static org.junit.Assert.assertTrue;
+
+// import org.junit.Before;
+// import org.junit.Test;
+
+
+// public class test extends SpringBootHelloWorldTests {
+
+// 	// @Autowired
+// 	// private WebApplicationContext webApplicationContext;
+
+// 	// private MockMvc mockMvc;
+
+// 	// @Before
+// 	// public void setup() {
+// 	// 	mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+// 	// }
+
+// 	// @Test
+// 	// public void testEmployee() throws Exception {
+// 	// 	mockMvc.perform(get("/employee")).andExpect(status().isOk())
+// 	// 			.andExpect(content().contentType("application/json;charset=UTF-8"))
+// 	// 			.andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
+// 	// 			.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
+
+// 	// }
+
+//   @Test
+// public void givenNumber_whenEven_thenTrue() {
+// 	int number=12;
+//     assertTrue(number % 2 == 0);
+// }
+// }
