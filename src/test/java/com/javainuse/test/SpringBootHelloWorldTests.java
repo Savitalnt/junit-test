2049cf4c83107ab1ce8b4da4f406ab7e849bc318
Savitalnt <111843797+Savitalnt@users.noreply.github.com>
2023-12-19 16:47:24 +0530

    Update SpringBootHelloWorldTests.java

--- src/test/java/com/javainuse/test/SpringBootHelloWorldTests.java
+++ src/test/java/com/javainuse/test/SpringBootHelloWorldTests.java
@@ -1,5 +1,5 @@
 package com.javainuse.test;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
