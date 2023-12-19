0decfaa0d55e6acdb70056417732063a18245cb3
Savitalnt <111843797+Savitalnt@users.noreply.github.com>
2023-12-19 15:23:58 +0530

    Update test.java

--- src/test/java/com/javainuse/test/test.java
+++ src/test/java/com/javainuse/test/test.java
@@ -1,11 +1,16 @@
-// import org.junit. Test;
-// import org.junit. Ignore;
-// import static org.junit.Assert.assertEquals;
+package com.javainuse.test;
 
-// public class BrowserStackJunit {
-// String message = "BrowserStack is the intended message";
-// @Test 
-// public void testMessage() {
-// System.out. println("Inside testMessage()");
-// assertEquals(message, "BrowserStack is the intended message");
-// }
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+  @Test
+public void givenNumber_whenEven_thenTrue() {
+	int number=12;
+    assertTrue(number % 2 == 0);
+}
