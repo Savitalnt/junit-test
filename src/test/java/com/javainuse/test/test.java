33e72ccac5b8202f5b3786269dd9acd940d1b69e
Savitalnt <111843797+Savitalnt@users.noreply.github.com>
2023-12-19 15:26:46 +0530

    Update test.java

--- src/test/java/com/javainuse/test/test.java
+++ src/test/java/com/javainuse/test/test.java
@@ -9,8 +9,31 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+
+public class test extends SpringBootHelloWorldTests {
+
+	// @Autowired
+	// private WebApplicationContext webApplicationContext;
+
+	// private MockMvc mockMvc;
+
+	// @Before
+	// public void setup() {
+	// 	mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	// }
+
+	// @Test
+	// public void testEmployee() throws Exception {
+	// 	mockMvc.perform(get("/employee")).andExpect(status().isOk())
+	// 			.andExpect(content().contentType("application/json;charset=UTF-8"))
+	// 			.andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
+	// 			.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
+
+	// }
+
   @Test
 public void givenNumber_whenEven_thenTrue() {
 	int number=12;
     assertTrue(number % 2 == 0);
+}
 }
