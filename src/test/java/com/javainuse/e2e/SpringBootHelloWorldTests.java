d2c2a13ba686c080bae91135323f84ddf427ebaa
Savitalnt <111843797+Savitalnt@users.noreply.github.com>
2023-12-19 17:02:41 +0530

    Update SpringBootHelloWorldTests.java

--- src/test/java/com/javainuse/e2e/SpringBootHelloWorldTests.java
+++ src/test/java/com/javainuse/e2e/SpringBootHelloWorldTests.java
@@ -5,13 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+
+import org.testng.annotations.Test;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootHelloWorldTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	// @Test
+	// public void contextLoads() {
+	// }
         @Test
 	public void test() {
  	int number=12;
