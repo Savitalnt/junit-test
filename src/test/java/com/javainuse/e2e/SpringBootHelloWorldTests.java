362c499c97e076ccd955a872638ebba2b311f5d1
Savitalnt <111843797+Savitalnt@users.noreply.github.com>
2023-12-19 16:51:23 +0530

    Create SpringBootHelloWorldTests.java

---
+++ src/test/java/com/javainuse/e2e/SpringBootHelloWorldTests.java
@@ -0,0 +1,20 @@
+package com.javainuse.e2e;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHelloWorldTests {
+
+	@Test
+	public void contextLoads() {
+	}
+        @Test
+	public void test() {
+ 	int number=12;
+    assertTrue(number % 2 == 0);
+ }
+}
